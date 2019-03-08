c1efcc9cb076005901beb85c75903614b6c2ffdb
Sandy Graham <sandy.gavlick.graham@gmail.com>
2019-03-08 16:14:09 -0600

    Ready to work on withdrawal and deposit

--- checkbook.py
+++ checkbook.py
@@ -1,14 +1,15 @@
-def view_current_balance(trans_by_field):
+def view_current_balance(list_of_transactions):
     # choice 1
     balance = 0
-    for i in range(0,len(trans_by_field)):
-        balance += float(trans_by_field[i][1])
+    for i in range(0,len(list_of_transactions)):
+        balance += float(list_of_transactions[i][1])
     print(f'Your balance is ${balance:.2f}.')
     print()
 
 
-def record_withdrawal(transactions):
+def record_withdrawal(all_transactions, transaction_number):
     # choice 2
+    transaction_number += 1
     debit_input = input('How much is the withdrawal? ')
     # check for valid debit
     while not debit_input.isdigit() or int(debit_input) <= 0:
@@ -18,8 +19,9 @@ def record_withdrawal(transactions):
     print()
 
 
-def record_deposit(transactions):
+def record_deposit(all_transactions, transaction_number):
     # choice 3
+    transaction_number += 1
     credit_input = input('How much is the deposit? ')
     # check for valid deposit
     while not credit_input.isdigit():
@@ -35,18 +37,20 @@ with open('random_data_python_group_project.txt') as transactions_file:
     whole_file = transactions_file.readlines()
 headings = whole_file[0]
 
-transactions = whole_file[1:]
+all_transactions = whole_file[1:]
 
-trans_by_field = []
+list_of_transactions = []
 
-for trans in transactions:
-    trans_by_field.append(trans.split('\t'))
+for trans in all_transactions:
+    list_of_transactions.append(trans.split('\t'))
+
+holder = list_of_transactions[len(list_of_transactions)-1][0]
+transaction_number = int(holder)
 
 print('~~~ Welcome to your terminal checkbook! ~~~')
 print()
 again = True
 while again:
-    choice_input = '4'
     print('What would you like to do?')
     print('1) view current balance')
     print('2) record a debit (withdraw)')
@@ -64,10 +68,10 @@ while again:
             again = False
         else:
             if choice == 1:
-                view_current_balance(trans_by_field)
+                view_current_balance(list_of_transactions)
             elif choice == 2:
-                record_withdrawal(trans_by_field)
+                record_withdrawal(list_of_transactions, transaction_number)
             elif choice == 3:
-                record_deposit(trans_by_field)
+                record_deposit(list_of_transactions, transaction_number)
 
 print('Thanks, have a great day!')
