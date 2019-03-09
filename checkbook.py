121d2b1e82db983eae140b3f6c7a5b5c8b4d5569
Sandy Graham <sandy.gavlick.graham@gmail.com>
2019-03-09 15:47:06 -0600

    fixed all four basic transactions

--- checkbook.py
+++ checkbook.py
@@ -2,7 +2,6 @@ def view_current_balance(list_of_transactions):
     # choice 1
     balance = 0
     for i in range(0,(len(list_of_transactions))):
-        print(i, list_of_transactions[i][0], list_of_transactions[i][1])
         balance += float(list_of_transactions[i][1])
     print(f'Your balance is ${balance:.2f}.')
     print()
@@ -14,23 +13,19 @@ def record_withdrawal(all_transactions, transaction_number):
     debit_input = input('How much is the withdrawal? ')
     # check for valid debit
     debit_parts = ''.join(debit_input.split('.'))
-    print(debit_parts)
     while not debit_parts.isdigit() or int(debit_parts) <= 0:
         debit_input = input('Please re-enter your withdrawal, using only digits: ')
-    debit = float(debit_input)
-    print(f'You entered ${debit:.2f}.') # remove later
     
-    category = input('What category would you like to enter for your withdrawal (1-10)?')
-    while not category.isdigit() or int(category) < 1 or int(category) > 10:
-        category = input('What category would you like to enter for your withdrawal (1-10)?')
+    category = input('What category would you like to enter for your withdrawal (1-9)? ')
+    while not category.isdigit() or int(category) < 1 or int(category) > 9:
+        category = input('Please enter a valid category (1, 2, 3, 4, 5, 6, 7, 8, or 9): ')
     print()
 
     day = datetime.datetime.now().strftime("%Y/%m/%d")
     time = datetime.datetime.now().strftime("%H:%M:%S")
 
-    this_transaction = str(str(transaction_number)+'\t' +'-'+debit_input+'\t'+'\t' +category+'\t' + day+'\t' + time+'\n')
+    this_transaction = str(str(transaction_number)+'\t' +'-'+debit_input+'\t' +category+'\t' + day+'\t' + time+'\n')
     all_transactions.append(this_transaction)
-    print(all_transactions[-3:])
 
     with open('random_data_python_group_project.txt', 'a') as f:
         f.write(str(this_transaction))
@@ -42,16 +37,26 @@ def record_deposit(all_transactions, transaction_number):
     transaction_number += 1
     credit_input = input('How much is the deposit? ')
     # check for valid deposit
-    while not credit_input.isdigit():
-        credit_input = input('Please re-enter your debit, using only digits: ')
-    credit = float(credit_input)
-    print(f'You entered ${credit:.2f}.')
-    print()
+    credit_parts = ''.join(credit_input.split('.'))
+    while not credit_parts.isdigit() or int(credit_parts) <= 0:
+        credit_input = input('Please re-enter your deposit, using only digits: ')
+    
+    category = '10'
+
+    day = datetime.datetime.now().strftime("%Y/%m/%d")
+    time = datetime.datetime.now().strftime("%H:%M:%S")
+
+    this_transaction = str(str(transaction_number)+'\t' + credit_input+'\t' +category+'\t' + day+'\t' + time+'\n')
+    all_transactions.append(this_transaction)
+
+    with open('random_data_python_group_project.txt', 'a') as f:
+        f.write(str(this_transaction))
+
 
 
 # # @@@@@@@ MAIN @@@@@@@@
-# import time
-# import datetime
+import time
+import datetime
 # read file of transaction and set up lists
 with open('random_data_python_group_project.txt') as transactions_file:
     whole_file = transactions_file.readlines()
@@ -135,8 +140,8 @@ while again:
                 record_deposit(list_of_transactions, transaction_number)
             elif choice == 5:
                 date_search(update)
-            elif choice == 6:
-                category_search(category_sample)
+            # elif choice == 6:
+            #     category_search(category_sample)
 
 print('Thanks, have a great day!')
 
