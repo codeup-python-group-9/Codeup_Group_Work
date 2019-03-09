08955b32dc5997ea70afe67c9a126d8980a4d43d
Joseph Burton <josephburton06@gmail.com>
2019-03-09 15:18:52 -0600

    Update date function to use our data

--- checkbook.py
+++ checkbook.py
@@ -1,7 +1,6 @@
 def view_current_balance(list_of_transactions):
     # choice 1
     balance = 0
-    print(list_of_transactions[0:4])
     for i in range(0,(len(list_of_transactions))):
         print(i, list_of_transactions[i][0], list_of_transactions[i][1])
         balance += float(list_of_transactions[i][1])
@@ -9,45 +8,45 @@ def view_current_balance(list_of_transactions):
     print()
 
 
-# def record_withdrawal(all_transactions, transaction_number):
-#     # choice 2
-#     transaction_number += 1
-#     debit_input = input('How much is the withdrawal? ')
-#     # check for valid debit
-#     debit_parts = ''.join(debit_input.split('.'))
-#     print(debit_parts)
-#     while not debit_parts.isdigit() or int(debit_parts) <= 0:
-#         debit_input = input('Please re-enter your withdrawal, using only digits: ')
-#     debit = float(debit_input)
-#     print(f'You entered ${debit:.2f}.') # remove later
+def record_withdrawal(all_transactions, transaction_number):
+    # choice 2
+    transaction_number += 1
+    debit_input = input('How much is the withdrawal? ')
+    # check for valid debit
+    debit_parts = ''.join(debit_input.split('.'))
+    print(debit_parts)
+    while not debit_parts.isdigit() or int(debit_parts) <= 0:
+        debit_input = input('Please re-enter your withdrawal, using only digits: ')
+    debit = float(debit_input)
+    print(f'You entered ${debit:.2f}.') # remove later
     
-#     category = input('What category would you like to enter for your withdrawal (1-10)?')
-#     while not category.isdigit() or int(category) < 1 or int(category) > 10:
-#         category = input('What category would you like to enter for your withdrawal (1-10)?')
-#     print()
+    category = input('What category would you like to enter for your withdrawal (1-10)?')
+    while not category.isdigit() or int(category) < 1 or int(category) > 10:
+        category = input('What category would you like to enter for your withdrawal (1-10)?')
+    print()
 
-#     day = datetime.datetime.now().strftime("%Y/%m/%d")
-#     time = datetime.datetime.now().strftime("%H:%M:%S")
+    day = datetime.datetime.now().strftime("%Y/%m/%d")
+    time = datetime.datetime.now().strftime("%H:%M:%S")
 
-#     this_transaction = str(str(transaction_number)+'\t' +'-'+debit_input+'\t'+'\t' +category+'\t' + day+'\t' + time+'\n')
-#     all_transactions.append(this_transaction)
-#     print(all_transactions[-3:])
+    this_transaction = str(str(transaction_number)+'\t' +'-'+debit_input+'\t'+'\t' +category+'\t' + day+'\t' + time+'\n')
+    all_transactions.append(this_transaction)
+    print(all_transactions[-3:])
 
-#     with open('random_data_python_group_project.txt', 'a') as f:
-#         f.write(str(this_transaction))
+    with open('random_data_python_group_project.txt', 'a') as f:
+        f.write(str(this_transaction))
 
 
 
-# def record_deposit(all_transactions, transaction_number):
-#     # choice 3
-#     transaction_number += 1
-#     credit_input = input('How much is the deposit? ')
-#     # check for valid deposit
-#     while not credit_input.isdigit():
-#         credit_input = input('Please re-enter your debit, using only digits: ')
-#     credit = float(credit_input)
-#     print(f'You entered ${credit:.2f}.')
-#     print()
+def record_deposit(all_transactions, transaction_number):
+    # choice 3
+    transaction_number += 1
+    credit_input = input('How much is the deposit? ')
+    # check for valid deposit
+    while not credit_input.isdigit():
+        credit_input = input('Please re-enter your debit, using only digits: ')
+    credit = float(credit_input)
+    print(f'You entered ${credit:.2f}.')
+    print()
 
 
 # # @@@@@@@ MAIN @@@@@@@@
@@ -69,11 +68,27 @@ for trans in all_transactions:
 holder = list_of_transactions[len(list_of_transactions)-1][0]
 transaction_number = int(holder)
 
+dates_of_transactions = []
 
-# test_date = ['2019/03/08', '2019/02/03', '2019/03/01', '2019/02/23']
-# update = []
-# for date in test_date:
-#     update.append(int(date.replace('/','')))
+for trans in list_of_transactions:
+    dates_of_transactions.append(trans[-2])
+
+
+update = []
+for date in dates_of_transactions:
+    update.append(int(date.replace('/','')))
+
+def date_search(update):
+    input_date_min = input('What date would you like to start with? Enter as YYYY/MM/DD: ')
+    date_min = int(input_date_min.replace('/',''))
+    print('Your start date is '+str(date_min))
+    input_date_max = input('What is your end date:  Enter as YYYY/MM/DD: ')
+    date_max = int(input_date_max.replace('/',''))
+    your_date_range = []
+    for date in update:
+        if date >= date_min and date <= date_max:
+            your_date_range.append(date)
+    print(your_date_range)
 
 # category_sample = ['2','3','4','5','8','3','9','3','8','4','2','9','4','8','2','7']
 # int_category_sample = []
@@ -90,21 +105,6 @@ transaction_number = int(holder)
 #         if user_category_input in int_category_sample:
 #             user_category_input_int.append(user_category_input)
 #     print(user_category_input_int)
-
-
-# def date_search(update):
-#     input_date_min = input('What date would you like to start with? Enter as YYYY/MM/DD: ')
-#     date_min = int(input_date_min.replace('/',''))
-#     print('Your start date is '+str(date_min))
-#     input_date_max = input('What is your end date:  Enter as YYYY/MM/DD: ')
-#     date_max = int(input_date_max.replace('/',''))
-#     your_date_range = []
-#     for date in update:
-#         if date >= date_min and date <= date_max:
-#             your_date_range.append(date)
-#     print(your_date_range)
-
-
 
 
 print('~~~ Welcome to your terminal checkbook! ~~~')
@@ -129,14 +129,14 @@ while again:
         else:
             if choice == 1:
                 view_current_balance(list_of_transactions)
-            # elif choice == 2:
-            #     record_withdrawal(list_of_transactions, transaction_number)
-            # elif choice == 3:
-            #     record_deposit(list_of_transactions, transaction_number)
-            # elif choice == 5:
-            #     date_search(update)
-            # elif choice == 6:
-            #     category_search(category_sample)
+            elif choice == 2:
+                record_withdrawal(list_of_transactions, transaction_number)
+            elif choice == 3:
+                record_deposit(list_of_transactions, transaction_number)
+            elif choice == 5:
+                date_search(update)
+            elif choice == 6:
+                category_search(category_sample)
 
 print('Thanks, have a great day!')
 
